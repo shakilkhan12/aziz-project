a7fe098cf407319bb8a1b8defddd840853cfaa3c
Shakil Khan <shakilkhan621@gmail.com>
2021-06-06 10:31:09 +0500

    upload

--- components/nav.php
+++ components/nav.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container">
-    <a class="navbar-brand" href="#">Logo</a>
+    <a class="navbar-brand" href="#">Tools Selling</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
---
+++ home.php
@@ -0,0 +1,200 @@
+<?php
+include "./init.php";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/app.css">
+</head>
+<body>
+<?php include_once "./components/nav.php";  ?>
+<div class="container mt-4">
+  <div class="alert alert-info">Top sellers list</div>
+  <div class="row">
+  <div class="col-md-1">
+  <div class="top-seller">
+  <div class="text-center">
+  <img src="./assets/images/1.jpg" class="rounded img-circle" alt="...">
+  <p class="mt-1 size-13">Asad Khan</p>
+</div>
+  </div>
+  </div>
+
+  <div class="col-md-1">
+  <div class="top-seller">
+  <div class="text-center">
+  <img src="./assets/images/2.jpg" class="rounded img-circle" alt="...">
+  <p class="mt-1 size-13">Kamran Khan</p>
+</div>
+  </div>
+  </div>
+
+  <div class="col-md-1">
+  <div class="top-seller">
+  <div class="text-center">
+  <img src="./assets/images/3.jpg" class="rounded img-circle" alt="...">
+  <p class="mt-1 size-13">Javed Khan</p>
+</div>
+  </div>
+  </div>
+
+  <div class="col-md-1">
+  <div class="top-seller">
+  <div class="text-center">
+  <img src="./assets/images/4.jpg" class="rounded img-circle" alt="...">
+  <p class="mt-1 size-13">Anas</p>
+</div>
+  </div>
+  </div>
+
+
+  <div class="col-md-1">
+  <div class="top-seller">
+  <div class="text-center">
+  <img src="./assets/images/5.jpg" class="rounded img-circle" alt="...">
+  <p class="mt-1 size-13">John deo</p>
+</div>
+  </div>
+  </div>
+
+
+  <div class="col-md-1">
+  <div class="top-seller">
+  <div class="text-center">
+  <img src="./assets/images/6.jpg" class="rounded img-circle" alt="...">
+  <p class="mt-1 size-13">Smith White</p>
+</div>
+  </div>
+  </div>
+
+  </div>
+
+  <!-- <div class="alert alert-warning">Tools list</div> -->
+  <div class="row">
+  <div class="col-md-3 mt-5">
+    <div class="image-box">
+  <img src="./assets/images/tools/tool1.jpg" class="img-fluid" alt="...">
+  </div>
+  <h5 class="mt-3 tool-heading">SEO Best Tool</h5>
+  <div class="mt-2">
+  <span>Rs</span>
+  <span class="fs-4 bold text-info">$600.00</span>
+  </div>
+  <div class="mt-2">
+    <a href="" class="btn btn-primary btn-sm">Details</a>
+  </div>
+  </div>
+
+
+  <div class="col-md-3 mt-5">
+    <div class="image-box">
+  <img src="./assets/images/tools/tool2.jpg" class="img-fluid" alt="...">
+  </div>
+  <h5 class="mt-3">Bootstrap Tools</h5>
+  <div class="mt-2">
+  <span>Rs</span>
+  <span class="fs-4 bold text-info">$700.00</span>
+  </div>
+  <div class="mt-2">
+    <a href="" class="btn btn-primary btn-sm">Details</a>
+  </div>
+  </div>
+
+  <div class="col-md-3 mt-5">
+    <div class="image-box">
+  <img src="./assets/images/tools/tool3.jpg" class="img-fluid" alt="...">
+  </div>
+  <h5 class="mt-3">SEO Best Tool</h5>
+  <div class="mt-2">
+  <span>Rs</span>
+  <span class="fs-4 bold text-info">$200.00</span>
+  </div>
+  <div class="mt-2">
+    <a href="" class="btn btn-primary btn-sm">Details</a>
+  </div>
+  </div>
+
+  <div class="col-md-3 mt-5">
+    <div class="image-box">
+  <img src="./assets/images/tools/tool4.jpg" class="" alt="...">
+  </div>
+  <h5 class="mt-3">SEO Best Tool</h5>
+  <div class="mt-2">
+  <span>Rs</span>
+  <span class="fs-4 bold text-info">$600.00</span>
+  </div>
+  <div class="mt-2">
+    <a href="" class="btn btn-primary btn-sm">Details</a>
+  </div>
+  </div>
+
+  <div class="col-md-3 mt-5">
+    <div class="image-box">
+  <img src="./assets/images/tools/tool5.jpg" class="" alt="...">
+  </div>
+  <h5 class="mt-3">SEO Best Tool</h5>
+  <div class="mt-2">
+  <span>Rs</span>
+  <span class="fs-4 bold text-info">$600.00</span>
+  </div>
+  <div class="mt-2">
+    <a href="" class="btn btn-primary btn-sm">Details</a>
+  </div>
+  </div>
+
+  <div class="col-md-3 mt-5">
+    <div class="image-box">
+  <img src="./assets/images/tools/tool6.jpg" class="" alt="...">
+  </div>
+  <h5 class="mt-3">SEO Best Tool</h5>
+  <div class="mt-2">
+  <span>Rs</span>
+  <span class="fs-4 bold text-info">$600.00</span>
+  </div>
+  <div class="mt-2">
+    <a href="" class="btn btn-primary btn-sm">Details</a>
+  </div>
+  </div>
+
+  <div class="col-md-3 mt-5">
+    <div class="image-box">
+  <img src="./assets/images/tools/tool7.jpg" class="" alt="...">
+  </div>
+  <h5 class="mt-3">SEO Best Tool</h5>
+  <div class="mt-2">
+  <span>Rs</span>
+  <span class="fs-4 bold text-info">$600.00</span>
+  </div>
+  <div class="mt-2">
+    <a href="" class="btn btn-primary btn-sm">Details</a>
+  </div>
+  </div>
+
+  <div class="col-md-3 mt-5">
+    <div class="image-box">
+  <img src="./assets/images/tools/tool8.jpg" class="" alt="...">
+  </div>
+  <h5 class="mt-3">SEO Best Tool</h5>
+  <div class="mt-2">
+  <span>Rs</span>
+  <span class="fs-4 bold text-info">$600.00</span>
+  </div>
+  <div class="mt-2">
+    <a href="" class="btn btn-primary btn-sm">Details</a>
+  </div>
+  </div>
+
+  
+
+
+  </div>
+
+</div>
+</body>
+</html>
